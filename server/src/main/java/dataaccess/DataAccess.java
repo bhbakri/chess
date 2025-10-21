0654f1466dee9e89bd98b704abfa86913c0d5dda
bhbakri <bakrihbassem@gmail.com>
2025-10-21 00:18:10 -0600

    Dataaccess code

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,4 +1,28 @@
 package dataaccess;
 
-public class DataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+    // system
+    void clear() throws DataAccessException;
+
+    // for users
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+    // authenticator
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    // the game (you lose ;))
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
 }
