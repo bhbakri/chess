9dc02ac6dcf2a8caf0ea156ba6d1aabf2babd065
bhbakri <bakrihbassem@gmail.com>
2025-10-21 22:31:35 -0600

    Finishing dataaccess

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -24,65 +24,75 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     //user functions
+
     @Override
     public void createUser(UserData user) throws DataAccessException {
         if (user == null || user.username() == null) {
             throw new DataAccessException("null user");
         }
-
-        users.put(user.username(), user);
+        // Dont remove existent users
+        users.putIfAbsent(user.username(), user);
     }
 
     @Override
-    public UserData getUser(String username) throws DataAccessException {
+    public UserData getUser(String username) {
+        if (username == null) return null;
         return users.get(username);
     }
 
     //authentication
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
-        if (auth == null) throw new DataAccessException("null auth");
+        if (auth == null || auth.authToken() == null) {
+            throw new DataAccessException("null auth");
+        }
         auths.put(auth.authToken(), auth);
     }
 
     @Override
-    public AuthData getAuth(String authToken) throws DataAccessException {
+    public AuthData getAuth(String authToken) {
+        if (authToken == null) return null;
         return auths.get(authToken);
     }
 
     @Override
-    public void deleteAuth(String authToken) throws DataAccessException {
-        auths.remove(authToken);
+    public void deleteAuth(String authToken) {
+        if (authToken != null) {
+            auths.remove(authToken);
+        }
     }
 
     @Override
     public int createGame(GameData game) throws DataAccessException {
         if (game == null) throw new DataAccessException("null game");
         int id = nextGameId.getAndIncrement();
-
-        //temp
-        games.put(id, new GameData(
+        GameData toStore = new GameData(
                 id,
                 game.whiteUsername(),
                 game.blackUsername(),
                 game.gameName(),
                 game.game()
-        ));
-
+        );
+        games.put(id, toStore);
         return id;
     }
 
     @Override
-    public GameData getGame(int gameID) throws DataAccessException {
+    public GameData getGame(int gameID) {
         return games.get(gameID);
     }
 
     @Override
-    public Collection<GameData> listGames() throws DataAccessException {
-        return games.values();
+    public Collection<GameData> listGames() {
+        return Collections.unmodifiableCollection(games.values());
     }
 
     @Override
     public void updateGame(GameData game) throws DataAccessException {
+        if (game == null) throw new DataAccessException("null game");
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("no such game: " + game.gameID());
+        }
+        games.put(game.gameID(), game);
     }
 }
