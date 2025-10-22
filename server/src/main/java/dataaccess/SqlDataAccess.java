9f0d5ecff634f0fbbf08bddc5a01e9430ace60cb
bhbakri <bakrihbassem@gmail.com>
2025-10-22 10:25:35 -0600

    starting DA

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -9,5 +9,25 @@ import chess.ChessGame;
 import java.sql.*;
 import java.util.*;
 
-public class SqlDataAccess {
+public class SqlDataAccess implements DataAccess {
+
+    private final DatabaseManager db;
+    private final Gson gson = new Gson();
+
+    public SqlDataAccess(DatabaseManager db) { this.db = db; }
+
+    @Override
+    public void clear() throws DataAccessException {
+        try (var conn = db.getConnection();
+             var s1 = conn.createStatement();
+             var s2 = conn.createStatement();
+             var s3 = conn.createStatement()) {
+            s1.executeUpdate("DELETE FROM auth;");
+            s2.executeUpdate("DELETE FROM games;");
+            s3.executeUpdate("DELETE FROM users;");
+            conn.commit();
+        } catch (Exception e) {
+            throw new DataAccessException("clear failed: " + e.getMessage());
+        }
+    }
 }
