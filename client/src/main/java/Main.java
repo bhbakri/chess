48b96e32973284d28799120c3030363950921873
bhbakri <bakrihbassem@gmail.com>
2025-11-09 21:58:19 -0700

    Update Main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,6 +4,6 @@ import chess.ChessPiece;
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("I work!" + piece);
     }
 }
